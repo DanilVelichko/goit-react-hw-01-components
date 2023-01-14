0824334b0ebfd44454327ca9ff0f37d5ddc12fc1
DanilVelichko <velichko.danil@gmail.com>
2023-01-14 21:57:00 +0100

    Profile done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import user from './components/profile/user.json'
+import {Profile} from './components/profile/user-profile'; 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+<Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+  followers={user.stats.followers}
+  views={user.stats.views}
+  likes={user.stats.likes} />
+        
+
+
   </React.StrictMode>
 );
+
+
+
+// ReactDOM.createRoot(document.querySelector('#root')).render(userRender());
