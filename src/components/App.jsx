b60171807d4e9028ff4a9cc50a2a10f6cdcd62a0
DanilVelichko <105587925+DanilVelichko@users.noreply.github.com>
2023-01-16 17:13:15 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import user from '../data/user.json'
 import data from '../data/data.json'
 import friends from '../data/friends.json'
 import transactions  from '../data/transactions.json'
-import Profile from './Profile/User-profile'; 
+import Profile from './profile/User-profile'; 
 import StatisticsList from './StatisticsList/StatisticsList';
 import FriendList from './FriendList/FriendList';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
